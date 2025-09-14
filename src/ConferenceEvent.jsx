ceca30900c43690684aa2528e4580660a5f95417
wswim25 <upkeep-perky-5d@icloud.com>
2025-09-14 13:33:57 -0400

    Added initial state, AVSlice and AV totalcost

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -3,13 +3,16 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
+
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
-
+    const avTotalCost = calculateTotalCost("av");
     
     const handleToggleItems = () => {
         console.log("handleToggleItems called");
@@ -28,11 +31,13 @@ const ConferenceEvent = () => {
           dispatch(decrementQuantity(index));
         }
       };
-    const handleIncrementAvQuantity = (index) => {
-    };
+const handleIncrementAvQuantity = (index) => {
+    dispatch(incrementAvQuantity(index));
+};
 
-    const handleDecrementAvQuantity = (index) => {
-    };
+const handleDecrementAvQuantity = (index) => {
+    dispatch(decrementAvQuantity(index));
+};
 
     const handleMealSelection = (index) => {
        
@@ -47,15 +52,19 @@ const ConferenceEvent = () => {
     const ItemsDisplay = ({ items }) => {
 
     };
-    const calculateTotalCost = (section) => {
-        let totalCost = 0;
-        if (section === "venue") {
-          venueItems.forEach((item) => {
-            totalCost += item.cost * item.quantity;
-          });
-        }
-        return totalCost;
-      };
+ const calculateTotalCost = (section) => {
+    let totalCost = 0;
+    if (section === "venue") {
+      venueItems.forEach((item) => {
+        totalCost += item.cost * item.quantity;
+      });
+    } else if (section === "av") {
+      avItems.forEach((item) => {
+        totalCost += item.cost * item.quantity;
+      });
+    }
+    return totalCost;
+  };
     const venueTotalCost = calculateTotalCost("venue");
 
     const navigateToProducts = (idType) => {
@@ -162,6 +171,21 @@ const ConferenceEvent = () => {
                                 <div className="total_cost">Total Cost:</div>
 
                             </div>
+                            {avItems.map((item, index) => (
+    <div className="av_data venue_main" key={index}>
+        <div className="img">
+            <img src={item.img} alt={item.name} />
+        </div>
+    <div className="text"> {item.name} </div>
+    <div> ${item.cost} </div>
+        <div className="addons_btn">
+            <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+            <span className="quantity-value">{item.quantity}</span>
+            <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+        </div>
+    </div>
+))}
+<div className="total_cost">Total Cost: {avTotalCost}</div>
 
                             {/* Meal Section */}
 
--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReducer from './avSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
   },
 });
